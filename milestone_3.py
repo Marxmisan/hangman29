c105ffbc91233c491157850e6c7d971c4223794a
misan madamedon <marxmisan83@yahoo.com>
2023-12-04 14:35:23 +0000

    uploading milestone 3 a more cleaner version

--- milestone_3.py
+++ milestone_3.py
@@ -1,13 +1,24 @@
 import random
 
-word_list = ['mango','orange','apple','plum','grape']
-print(word_list)
+def create_word_list():
+    word_list = ['mango','orange','apple','plum','grape']
+    print(word_list)
+    return word_list
+
+def comp_choice_guess(word_list):
+    word = random.choice(word_list)
+    print(word)
+    guess = input('please enter a sigle letter  ')
+    if len(guess) == 1 and type(guess) == str:
+      print('Good guess')
+    else:
+      print('Oops! That is not a valid input')
+
+word_list = create_word_list()
+comp_choice_guess(word_list)
 
 
-word = random.choice(word_list)
-print(word)
-guess = input('please enter a sigle letter  ')
-if len(guess) == 1 and type(guess) == str:
-    print('Good guess')
-else:
-    print('Oops! That is not a valid input')
+
+
+
+
