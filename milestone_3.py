01be969775495b4aeb09f0e943f1db98cd0caf7f
misan madamedon <marxmisan83@yahoo.com>
2023-12-04 15:45:58 +0000

    a cleaner version

--- milestone_3.py
+++ milestone_3.py
@@ -2,20 +2,26 @@ import random
 
 def create_word_list():
     word_list = ['mango','orange','apple','plum','grape']
-    print(word_list)
+    # print(word_list)
     return word_list
 
 def comp_choice_guess(word_list):
     word = random.choice(word_list)
-    print(word)
-    guess = input('please enter a sigle letter  ')
-    if len(guess) == 1 and type(guess) == str:
-      print('Good guess')
-    else:
-      print('Oops! That is not a valid input')
+    # print(word)
+    guess = input('please enter a single letter  ')
+    return guess
 
-word_list = create_word_list()
-comp_choice_guess(word_list)
+xx=1
+while xx < 6:
+   word_list = create_word_list()
+   guess = comp_choice_guess(word_list)
+   x = guess.isalpha()
+
+   if len(guess)==1 and  x == True:
+     break
+   else:
+       print("Invalid letter. Please, enter a single alphabetical character.")
+
 
 
 
